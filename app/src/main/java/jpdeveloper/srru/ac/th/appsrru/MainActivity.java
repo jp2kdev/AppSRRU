22544c27be5fac1821b3f79fa025ea8a0125387f
Mr.Kosin Sanannam <kokoko704@gmail.com>
2018-04-25 14:08:40 +0700

    Initial commit

---
+++ app/src/main/java/jpdeveloper/srru/ac/th/appsrru/MainActivity.java
@@ -0,0 +1,28 @@
+package jpdeveloper.srru.ac.th.appsrru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jpdeveloper.srru.ac.th.appsrru.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Add Fragment (shift + command + enter)
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment())
+                    .commit();
+
+        }
+
+
+
+    } // Main Method
+
+}  //Main Class
