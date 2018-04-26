770a1ae4700da6b2f6335acba4f73aea00770ad7
Mr.Kosin Sanannam <kokoko704@gmail.com>
2018-04-26 17:01:49 +0700

    Adapter OK.

---
+++ app/src/main/java/jpdeveloper/srru/ac/th/appsrru/utility/FoodAdapter.java
@@ -0,0 +1,71 @@
+package jpdeveloper.srru.ac.th.appsrru.utility;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import jpdeveloper.srru.ac.th.appsrru.R;
+
+public class FoodAdapter extends BaseAdapter {
+
+    private Context context;
+
+    private String[] imageStrings, foodStrings, priceStrings, detailStrings;
+
+
+    public FoodAdapter(Context context,
+                       String[] imageStrings,
+                       String[] foodStrings,
+                       String[] priceStrings,
+                       String[] detailStrings) {
+        this.context = context;
+        this.imageStrings = imageStrings;
+        this.foodStrings = foodStrings;
+        this.priceStrings = priceStrings;
+        this.detailStrings = detailStrings;
+    }
+
+
+    @Override
+    public int getCount() {
+        return foodStrings.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.listview_food, parent, false);
+
+        TextView foodTextView = view.findViewById(R.id.txtFood);
+        TextView priceTextView = view.findViewById(R.id.txtPrice);
+        TextView detailTextView = view.findViewById(R.id.txtDetail);
+        TextView imageTextView = view.findViewById(R.id.imvFood);
+
+
+        foodTextView.setText(foodStrings[position]);
+        priceTextView.setText(priceStrings[position]);
+        detailTextView.setText(detailStrings[position]);
+
+        Picasso.get().load(imageStrings[position]).into((Target) imageTextView);
+
+
+        return null;
+    }
+}
